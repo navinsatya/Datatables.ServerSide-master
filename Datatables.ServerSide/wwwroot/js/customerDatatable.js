fc7ee89d3d21da6b3616038a55f0ee1eb426e9c4
navin_lgib2hj <navin_lgib2hj@LAPTOP-3GQK9K89>
2024-03-11 20:31:13 +0530

    Changes

--- Datatables.ServerSide/wwwroot/js/customerDatatable.js
+++ Datatables.ServerSide/wwwroot/js/customerDatatable.js
@@ -10,11 +10,11 @@
             "type": "POST",
             "datatype": "json"
         },
-        "columnDefs": [{
-            "targets": [0],
-            "visible": false,
-            "searchable": false
-        }],
+        //"columnDefs": [{
+        //    "targets": [0],
+        //    "visible": false,
+        //    "searchable": false
+        //}],
         "columns": [
             {
                 "data": "id", "name": "Id", "autoWidth": true, "render": function (data, type, full, meta) {
